26005ffa6147ec34028fc92386e77ed970c54c72
kaushik ganguly <kgsnipes@gmail.com>
2024-11-02 23:40:59 -0400

    using wrapper object to store cached query result

---
+++ src/main/kotlin/com/dws/tc/dto/CacheQueryObject.kt
@@ -0,0 +1,3 @@
+package com.dws.tc.dto
+
+data class CacheQueryObject(val id:String, val obj:Any,val queryListAttr:String)
--- src/main/kotlin/com/dws/tc/eh/TCache.kt
+++ src/main/kotlin/com/dws/tc/eh/TCache.kt
@@ -2,18 +2,20 @@ package com.dws.tc.eh
 
 interface TCache<T> {
 
+    //tenant operations
     fun createTenantCache(name:String,config:Map<String,String>)
     fun removeTenantCache(name:String)
 
-    fun get(tenant:String,entity:String,id:String,isQuery:Boolean):T?
+    //read operations
+    fun get(tenant:String,entity:String,id:String,isQuery: Boolean):T?
 
+    //write operations
     fun put(tenant:String,value:T,updateAsync:Boolean)
     fun put(tenant:String,entity:String,id:String,value:T,updateAsync:Boolean)
-
     fun putQueryResult(tenant:String,queryId:String,value: Any,listAttribute:String,updateAsync:Boolean)
 
+    //delete operations
     fun flushAll(tenant:String)
-
     fun remove(tenant:String,entity:String,id:String,isQuery:Boolean,updateAsync:Boolean)
 
     fun hasKey(tenant:String,entity:String,id:String,isQuery:Boolean):Boolean
